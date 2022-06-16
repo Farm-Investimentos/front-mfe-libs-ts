a93687c429d1280e0cc0db69eeb416e0d9a7baa2
viniciussecari <vinicius.secari@farminvestimentos.com.br>
2022-06-16 16:09:27 -0300

    feat: Anotations included

--- src/helpers/queryString/queryString.ts
+++ src/helpers/queryString/queryString.ts
@@ -2,6 +2,8 @@
  * Creates an query string based on an object and a list of keys used to transform the parameter
  * name into a parameter expected by the API
  * @module
+ * @param {any} filters - filters selected to add like querystring at URL
+ * @param {any} mappings - especific filter update
  */
 export default (filters: any, mappings: any) =>
 	Object.keys(filters)
