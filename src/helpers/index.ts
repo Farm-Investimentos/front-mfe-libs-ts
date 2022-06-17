266e937f0ccdbfeafe2f20ad733c002bc0d7a684
viniciussecari <vinicius.secari@farminvestimentos.com.br>
2022-06-17 14:32:54 -0300

    feat: SimpleBuilder funcionality included

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -6,11 +6,13 @@ import queryString from './queryString';
 import fileSaver from './fileSaver';
 import errorBuilder from './errorBuilder';
 import installComponents from './installComponents';
+import simpleBuilder from './simpleBuilder';
 
 export {
 	file,
 	queryString,
 	fileSaver,
 	errorBuilder,
-	installComponents
+	installComponents,
+	simpleBuilder
 };
