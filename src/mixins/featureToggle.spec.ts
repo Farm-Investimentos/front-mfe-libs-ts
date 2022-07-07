d670e04dd58c3fc526db8e9296f99f7acc9a4ecb
Christian Benseler FARM <christian.benseler@farminvestimentos.com.br>
2022-07-07 07:31:37 -0300

    refactor: remove mixins temp

--- src/mixins/featureToggle.spec.ts
+++
@@ -1,8 +0,0 @@
-import { flatObject } from './featureToggle';
-
-describe('flatObject', () => {
-	it('should flat an object methods', () => {
-		const flat = flatObject({ a: { b: { c: 1 } } });
-		expect(flat['a.b.c']).toEqual(1);
-	});
-});
