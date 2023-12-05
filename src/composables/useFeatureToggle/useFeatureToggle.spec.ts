3cae7dd14f80da3ff00c1922793bfc7d78ff6c4c
Rapha <roliveira@farmtech.com.br>
2023-12-05 14:25:04 -0300

    fix: pass client to loadFeatures function instead composable itself

--- src/composables/useFeatureToggle/useFeatureToggle.spec.ts
+++ src/composables/useFeatureToggle/useFeatureToggle.spec.ts
@@ -1,137 +1,150 @@
-import axios from "axios";
-import { useFeatureToggle } from "./useFeatureToggle";
+import axios from 'axios';
+import { useFeatureToggle } from './useFeatureToggle';
 
-jest.mock("axios");
+jest.mock('axios');
 
-const featureTestKey = 'featureTestKey'
+const featureTestKey = 'featureTestKey';
 
 // Important: this composable share state, that's why we need to clean `flatFeaturesRules.value`
 describe('useFeatureToggle composable', () => {
-  let mockedAxios = axios as jest.Mocked<typeof axios>;
+	let mockedAxios = axios as jest.Mocked<typeof axios>;
 
-  describe('feature toggle object composition', () => {
-    it('should test behavior of simple object', async () => {
-      const data = {
-        testKey: true,
-        testKey2: false
-      }
-  
-      mockedAxios.get.mockResolvedValue({ data })
-      
-      const { flatFeaturesRules, loadFeatures, isFeatureEnabled } = useFeatureToggle(mockedAxios);
-  
-      await loadFeatures(featureTestKey);
-  
-      expect(flatFeaturesRules.value).toStrictEqual(data);
-      expect(isFeatureEnabled('testKey')).toBe(data.testKey);
-      expect(isFeatureEnabled('testKey2')).toBe(data.testKey2);
-    });
-  
-    it('should test behavior of deep object', async () => {
-      const data = {
-        testKey1: {
-          feature: {
-            checkForSomething: true
-          }
-        },
-        testKey2: {
-          checkForAnotherThing: true
-        }
-      }
-  
-      mockedAxios.get.mockResolvedValue({ data })
-      
-      const { flatFeaturesRules, loadFeatures, isFeatureEnabled } = useFeatureToggle(mockedAxios);
-  
-      await loadFeatures(featureTestKey);
-  
-      expect(flatFeaturesRules.value).toStrictEqual({
-        'testKey1.feature.checkForSomething': true,
-        'testKey2.checkForAnotherThing': true
-      })
-      expect(isFeatureEnabled('testKey1.feature.checkForSomething')).toBe(data.testKey1.feature.checkForSomething);
-      expect(isFeatureEnabled('testKey2.checkForAnotherThing')).toBe(data.testKey2.checkForAnotherThing);
-    });
-  })
+	describe('feature toggle object composition', () => {
+		it('should test behavior of simple object', async () => {
+			const data = {
+				testKey: true,
+				testKey2: false,
+			};
 
-  describe('isLoading', () => {
-    it('should have loading false at first and after request', async () => {
-      const data = {
-        testKey: true,
-        testKey2: false
-      }
-  
-      mockedAxios.get.mockResolvedValue({ data })
-      
-      const { isLoading, loadFeatures } = useFeatureToggle(mockedAxios);
+			mockedAxios.get.mockResolvedValue({ data });
 
-      expect(isLoading.value).toBe(false);
+			const { flatFeaturesRules, loadFeatures, isFeatureEnabled } =
+				useFeatureToggle();
 
-      await loadFeatures(featureTestKey);
+			await loadFeatures(mockedAxios, featureTestKey);
 
-      expect(isLoading.value).toBe(false);
-    });
+			expect(flatFeaturesRules.value).toStrictEqual(data);
+			expect(isFeatureEnabled('testKey')).toBe(data.testKey);
+			expect(isFeatureEnabled('testKey2')).toBe(data.testKey2);
+		});
 
-    it('should have loading false after failed request and not have any rules', async () => {
-      const consoleSpy = jest.spyOn(console, 'error').mockImplementation(jest.fn);
-  
-      mockedAxios.get.mockRejectedValue(new Error('No data'))
-      
-      const { flatFeaturesRules, isLoading, loadFeatures, isFeatureEnabled } = useFeatureToggle(mockedAxios);
+		it('should test behavior of deep object', async () => {
+			const data = {
+				testKey1: {
+					feature: {
+						checkForSomething: true,
+					},
+				},
+				testKey2: {
+					checkForAnotherThing: true,
+				},
+			};
 
-      // Reset shared feature flags to test if they will be empty
-      flatFeaturesRules.value = {};
+			mockedAxios.get.mockResolvedValue({ data });
 
-      await loadFeatures(featureTestKey);
+			const { flatFeaturesRules, loadFeatures, isFeatureEnabled } =
+				useFeatureToggle();
 
-      expect(isLoading.value).toBe(false);
-      expect(consoleSpy).toHaveBeenCalledTimes(1);
-      expect(isFeatureEnabled('testKey')).toBeFalsy();
-      expect(flatFeaturesRules.value).toStrictEqual({});
-    });
-  });
+			await loadFeatures(mockedAxios, featureTestKey);
 
-  describe('validations', () => {
-    it('should return true for no provided key', async () => {
-      const data = {
-        testKey1: {
-          feature: {
-            checkForSomething: true
-          }
-        },
-        testKey2: {
-          checkForAnotherThing: true
-        }
-      }
-  
-      mockedAxios.get.mockResolvedValue({ data })
-      
-      const { loadFeatures, isFeatureEnabled } = useFeatureToggle(mockedAxios);
-  
-      await loadFeatures(featureTestKey);
+			expect(flatFeaturesRules.value).toStrictEqual({
+				'testKey1.feature.checkForSomething': true,
+				'testKey2.checkForAnotherThing': true,
+			});
+			expect(isFeatureEnabled('testKey1.feature.checkForSomething')).toBe(
+				data.testKey1.feature.checkForSomething,
+			);
+			expect(isFeatureEnabled('testKey2.checkForAnotherThing')).toBe(
+				data.testKey2.checkForAnotherThing,
+			);
+		});
+	});
 
-      expect(isFeatureEnabled('')).toBeTruthy();
-    });
+	describe('isLoading', () => {
+		it('should have loading false at first and after request', async () => {
+			const data = {
+				testKey: true,
+				testKey2: false,
+			};
 
-    it('should return false for an invalid key', async () => {
-      const data = {
-        testKey1: {
-          feature: {
-            checkForSomething: true
-          }
-        },
-        testKey2: {
-          checkForAnotherThing: true
-        }
-      }
-  
-      mockedAxios.get.mockResolvedValue({ data })
-      
-      const { loadFeatures, isFeatureEnabled } = useFeatureToggle(mockedAxios);
-  
-      await loadFeatures(featureTestKey);
+			mockedAxios.get.mockResolvedValue({ data });
 
-      expect(isFeatureEnabled('testKey3.feature.check')).toBeFalsy();
-    });
-  })
-})
+			const { isLoading, loadFeatures } = useFeatureToggle();
+
+			expect(isLoading.value).toBe(false);
+
+			await loadFeatures(mockedAxios, featureTestKey);
+
+			expect(isLoading.value).toBe(false);
+		});
+
+		it('should have loading false after failed request and not have any rules', async () => {
+			const consoleSpy = jest
+				.spyOn(console, 'error')
+				.mockImplementation(jest.fn);
+
+			mockedAxios.get.mockRejectedValue(new Error('No data'));
+
+			const {
+				flatFeaturesRules,
+				isLoading,
+				loadFeatures,
+				isFeatureEnabled,
+			} = useFeatureToggle();
+
+			// Reset shared feature flags to test if they will be empty
+			flatFeaturesRules.value = {};
+
+			await loadFeatures(mockedAxios, featureTestKey);
+
+			expect(isLoading.value).toBe(false);
+			expect(consoleSpy).toHaveBeenCalledTimes(1);
+			expect(isFeatureEnabled('testKey')).toBeFalsy();
+			expect(flatFeaturesRules.value).toStrictEqual({});
+		});
+	});
+
+	describe('validations', () => {
+		it('should return true for no provided key', async () => {
+			const data = {
+				testKey1: {
+					feature: {
+						checkForSomething: true,
+					},
+				},
+				testKey2: {
+					checkForAnotherThing: true,
+				},
+			};
+
+			mockedAxios.get.mockResolvedValue({ data });
+
+			const { loadFeatures, isFeatureEnabled } = useFeatureToggle();
+
+			await loadFeatures(mockedAxios, featureTestKey);
+
+			expect(isFeatureEnabled('')).toBeTruthy();
+		});
+
+		it('should return false for an invalid key', async () => {
+			const data = {
+				testKey1: {
+					feature: {
+						checkForSomething: true,
+					},
+				},
+				testKey2: {
+					checkForAnotherThing: true,
+				},
+			};
+
+			mockedAxios.get.mockResolvedValue({ data });
+
+			const { loadFeatures, isFeatureEnabled } = useFeatureToggle();
+
+			await loadFeatures(mockedAxios, featureTestKey);
+
+			expect(isFeatureEnabled('testKey3.feature.check')).toBeFalsy();
+		});
+	});
+});
--- src/composables/useFeatureToggle/useFeatureToggle.ts
+++ src/composables/useFeatureToggle/useFeatureToggle.ts
@@ -1,49 +1,51 @@
-import { ref } from "vue";
-import flatObject from "../../helpers/flatObject";
+import { ref } from 'vue';
+import flatObject from '../../helpers/flatObject';
 
-import type { AxiosInstance } from "axios";
-import type { RecordListToFlatten } from "../../helpers/flatObject/types";
+import type { AxiosInstance } from 'axios';
+import type { RecordListToFlatten } from '../../helpers/flatObject/types';
 
 let flatFeaturesRules = ref<RecordListToFlatten<boolean>>({});
 
-export function useFeatureToggle (client: AxiosInstance) {
-  const isLoading = ref(false);
-  /**
-   * Fetch for feature flags in /features folder, which will be there after build of `front-mfe-orquestrador`
-   * or if we build `front-mfe-toogle-features`
-   * 
-   * @param name - The name of the file from where you want to get features
-   */
-  async function loadFeatures (name: string) {
-    isLoading.value = true
+export function useFeatureToggle() {
+	const isLoading = ref(false);
+	/**
+	 * Fetch for feature flags in /features folder, which will be there after build of `front-mfe-orquestrador`
+	 * or if we build `front-mfe-toogle-features`
+	 *
+	 * @param name - The name of the file from where you want to get features
+	 */
+	async function loadFeatures(client: AxiosInstance, name: string) {
+		isLoading.value = true;
 
-    try {
-      const { data } = await client.get<typeof flatFeaturesRules.value>(`/features/${name}.json`);
+		try {
+			const { data } = await client.get<typeof flatFeaturesRules.value>(
+				`/features/${name}.json`,
+			);
 
-      flatFeaturesRules.value = flatObject(data);
-    } catch (e) {
-      console.error(`Not able to load feature flags, Error: ${e}`);
-    } finally {
-      isLoading.value = false
-    }
-  }
+			flatFeaturesRules.value = flatObject(data);
+		} catch (e) {
+			console.error(`Not able to load feature flags, Error: ${e}`);
+		} finally {
+			isLoading.value = false;
+		}
+	}
 
-  /**
-   * Check if a feature key is present and active
-   * 
-   * @param key - Name of the key in a "flat-object" format, e.g: `wallet.notes.hasList`
-   */
-  function isFeatureEnabled (key: keyof typeof flatFeaturesRules.value) {
-    if (!key) {
-      return true;
-    }
-    return flatFeaturesRules.value[key] || false;
-  }
+	/**
+	 * Check if a feature key is present and active
+	 *
+	 * @param key - Name of the key in a "flat-object" format, e.g: `wallet.notes.hasList`
+	 */
+	function isFeatureEnabled(key: keyof typeof flatFeaturesRules.value) {
+		if (!key) {
+			return true;
+		}
+		return flatFeaturesRules.value[key] || false;
+	}
 
-  return {
-    isLoading,
-    flatFeaturesRules,
-    loadFeatures,
-    isFeatureEnabled
-  }
+	return {
+		isLoading,
+		flatFeaturesRules,
+		loadFeatures,
+		isFeatureEnabled,
+	};
 }
