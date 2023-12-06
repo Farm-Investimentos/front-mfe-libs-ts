d377e5f7b16567231b3c68b85147207b9ef7d1e6
Rapha <roliveira@farmtech.com.br>
2023-12-06 02:18:01 -0300

    test: add more coverage for exportHandler mixin

--- src/mixins/exportHandler/exportHandler.spec.ts
+++ src/mixins/exportHandler/exportHandler.spec.ts
@@ -1,11 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import exportHandler from '.';
 
 describe('exportHandler', () => {
+	beforeEach(() => jest.useFakeTimers());
+
+	afterEach(() => {
+		jest.runOnlyPendingTimers();
+		jest.useRealTimers();
+	});
+
 	it('should have methods', () => {
 		expect(exportHandler.methods).toBeDefined();
 	});
 
 	it('should have exportHandler method', () => {
 		expect(exportHandler.methods.exportHandler).toBeDefined();
+	});
+
+	it('should have been called window.dispatchEvent and callback after exportHandler call', () => {
+		const callback = jest.fn();
+		const windowSpy = jest.spyOn(window, 'dispatchEvent');
+
+		exportHandler.methods.exportHandler(callback);
+
+		jest.runAllTimers();
+
+		expect(windowSpy).toHaveBeenCalled();
+		expect(callback).toHaveBeenCalled();
 	});
 });
